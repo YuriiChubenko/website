51d0dfcd461af5d7be3f7f5aba930b8fef5ea70a
YuriiChubenko <yurii.chubenko@student.tugraz.at>
2024-06-18 23:07:19 +0200

    Made mobile version better

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
-/* pages/_app.js */
+// pages/_app.js
 
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,10 +6,6 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
     return (
         <> {/* Fragment to avoid unnecessary root element */}
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest" />
 
             <main className={styles.main}>
                 <GlassmorphicBox>
